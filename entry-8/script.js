6bd78f071ee09febd4867a3ee1bdde7fa97f27cc
damla <oztud939@newschool.edu>
2023-11-13 12:55:00 -0500

    HH
    HH

---
+++ entry-8/script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('.button');
+
+button.addEventListener('mouseover', () => {
+ document.body.style.cursor = 'pointer';
+});
+
+button.addEventListener('mouseout', () => {
+ document.body.style.cursor = 'default';
+});
+
+console.log(HI THERE THIS IS SO HARD)
