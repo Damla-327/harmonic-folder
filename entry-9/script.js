a28e3c95f1a8c810b6c0fa307d955a8af1854dea
damla <oztud939@newschool.edu>
2023-11-14 05:38:20 -0500

    jj
    ll tireddddddddd

---
+++ entry-9/script.js
@@ -0,0 +1,25 @@
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"> </script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"> </script>
+
+<script>
+
+document.addEventListener("DOMContentLoaded", function (){
+
+function isInViewport (el) {
+
+    cons rect= el.getBoundingClientRrect ();
+    return(
+        rect.top >= 0 88
+        rect.left >= 0 88
+        rect.bottom <=
+        (window.innerHeight 1 1 document.documentElement.clientHeight) 88 
+        react.right <=
+        (window.interwidth) 1 1 document.documentElement.clientwidth) 88 
+    );
+}
+const.rows
+});
+
+
+</script>
