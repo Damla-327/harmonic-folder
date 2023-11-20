5af07612aead52eb801f94d77f3cb907574ae7cc
damla <oztud939@newschool.edu>
2023-11-20 09:24:50 -0500

    gg
    zor

--- entry-9/script.js
+++ entry-9/script.js
@@ -1,25 +1,77 @@
-</div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"> </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"> </script>
 
-<script>
 
-document.addEventListener("DOMContentLoaded", function (){
 
-function isInViewport (el) {
+document.addEventListener("DOMContentLoaded", function () {
 
-    cons rect= el.getBoundingClientRrect ();
-    return(
-        rect.top >= 0 88
-        rect.left >= 0 88
-        rect.bottom <=
-        (window.innerHeight 1 1 document.documentElement.clientHeight) 88 
-        react.right <=
-        (window.interwidth) 1 1 document.documentElement.clientwidth) 88 
-    );
-}
-const.rows
+    function isInViewport (el) {
+        const rect= el.getBoundingClientRrect ();
+        return(
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+            react.right <=
+            (window.innerWidth || document.documentElement.clientWidth)
+            );
+
+    }
+
+    const rows= document.querySelectorAll(".row");
+    rows.forEach((row) => {
+        if(isInViewport(row)) {
+    
+            const img= row.querySelector("img");
+            if(row.querySelector(".left")) {
+                gsap.to(img, {
+                    clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+                });
+            } else{
+                gsap.to(img, {
+                    clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+                });
+            }
+        }
+    });
+
+
+    gsap.utils.toArray(".img-container.right img").forEach((img) => {
+        gsap.to(img, {
+            clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            scrollTrigger:{
+                trigger:img,
+                start: "top 75%",
+                end: "bottom 70%",
+                scrub: true,
+    
+            },
+        });
+    });
+
+    gsap.utils.toArray(".img-container.left img").forEach((img) => {
+        gsap.to(img, {
+            clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            scrollTrigger:{
+                trigger:img,
+                start: "top 75%",
+                end: "bottom 70%",
+                scrub: true,
+    
+            },
+        });
+    });
+
+    gsap.utils.toArray(".img-container p").forEach((text) =>{
+        gsap.from(text ,{
+            opacity: 0,
+            y: 20,
+            scrollTrigger: {
+                trigger: text,
+                start: "top 90%",
+                toggleActions: "play none none reverse",
+    
+            }
+        });
+    });
 });
-
-
-</script>
+</body>
+   </html>
